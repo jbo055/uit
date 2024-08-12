b7595dcab40fe5e0cb5d7d204c908c7272a56750
jbo055 <jbo055@uit.no>
2024-08-12 18:27:11 +0200

    Create ComputeArea.py

---
+++ Python/ExercisesFromBook/ComputeArea.py
@@ -0,0 +1,8 @@
+# write a program that will compute the area of a given radius
+
+radius = eval(input('Enter the radius of your area: '))
+pi = 3.14
+area = (radius ^ 2) * pi
+
+print('The area of ', radius, 'is ', area)
+
