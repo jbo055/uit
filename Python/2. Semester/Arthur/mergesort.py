f5d35b4ee9901f003b365d56de919350afda98de
jbo055 <jbo055@uit.no>
2025-03-20 08:50:47 +0100

    wq

--- Python/2. Semester/Arthur/mergesort.py
+++ Python/2. Semester/Arthur/mergesort.py
@@ -47,16 +47,18 @@ def quicksort(arr):
     if len(arr) <= 1:
         return arr
     
-    first = arr[0]
-    mid = arr[len(arr) // 2]
-    last = arr[-1]
-    pivot = sorted([first, mid, last])[1]
+    pivot = arr[0] # I set my pivot element to the first element, but it can be any element
 
-    less = [x for x in arr if x < pivot]
-    equal = [x for x in arr if x == pivot]
-    greater = [x for x in arr if x > pivot]
+    less = []
+    greater = []
 
-    return quicksort(less) + equal + quicksort(greater)
+    for x in arr[1:]:
+        if x <= pivot:
+            less.append(x)
+
+        else:
+            greater.append(x)
+    return quicksort(less) + [pivot] + quicksort(greater)
 
 arr = [38, 22, 25, 43, 9, 82]
 print(quicksort(arr))
