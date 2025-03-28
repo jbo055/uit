3889dbc43c6ed366cc43ea79ffcb7514c9ef6160
jbo055 <jbo055@uit.no>
2025-03-28 15:10:22 +0100

    ajk

--- Webdesign/app.py
+++ Webdesign/app.py
@@ -4,15 +4,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', active_page='index')
 
 @app.route('/aboutflask')
 def aboutflask():
-    return render_template('aboutflask.html')
+    return render_template('aboutflask.html', active_page='aboutflask')
 
 @app.route('/aboutme')
 def aboutme():
-    return render_template('aboutme.html')
+    return render_template('aboutme.html', active_page='aboutme')
 
 if __name__ == '__main__':
     app.run(debug=True)
